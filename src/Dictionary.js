b23277205f50533e5b683623e4d5d6f97b9dfb0b
mafalda-coelho <mafa.coel1@gmail.com>
2024-06-26 18:06:30 +0100

    added images

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Photos from "./Photos";
 import "./Dictionary.css";
 import Results from "./Results";
 
@@ -7,15 +8,24 @@ export default function Dictionary() {
   let [keyword, setKeyword] = useState("sunset");
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handleSheCodesResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
 
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let sheCodesApiKey = "bfebe20e536f474847ab0co02fa8tf40";
+    let sheCodesApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${sheCodesApiKey}`;
+    axios.get(sheCodesApiUrl).then(handleSheCodesResponse);
   }
 
   function handleSubmit(event) {
@@ -41,6 +51,7 @@ export default function Dictionary() {
           <div className="hint">suggested words: sunset, beach, wine...</div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
--- src/Phonetics.js
+++ src/Phonetics.js
@@ -5,7 +5,7 @@ export default function Phonetics(props) {
   return (
     <div className="Phonetic">
       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-        Hear
+        Hear it
       </a>
 
       <br />
